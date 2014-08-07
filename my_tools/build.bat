3d7b66821b45ac3d0e3cdc710402ea69c14caef3
utemkin <utemkin@inbox.ru>
2014-08-08 00:54:49 +0400

    My build options changed

--- my_tools/build.bat
+++ my_tools/build.bat
@@ -1,5 +1,5 @@
 @echo off
 set PYTHONPATH=%~dp0
 echo %PYTHONPATH%
-python ..\workspace_tools\build.py -m LPC1768_MINI_DK2 -tARM -v
-python ..\workspace_tools\project.py -m LPC1768_MINI_DK2 -i uvision -b -p 0
+python ..\workspace_tools\build.py -m LPC1768_MINI_DK2 -tARM -v -r -e -u -U -F
+python ..\workspace_tools\project.py -m LPC1768_MINI_DK2 -i uvision -b -p 83
